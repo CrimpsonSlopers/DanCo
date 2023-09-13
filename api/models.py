2ac16f678593dd1437736bd9f30d7b0b12f97528
John Rogers <69231460+CrimpsonSlopers@users.noreply.github.com>
2023-09-13 18:12:12 -0500

    Update models.py

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Landing(models.Model):
     description = models.CharField(max_length=20, null=False, unique=True)
-    cover_image = models.ImageField(default='default.jpg, upload_to='covers')
+    cover_image = models.ImageField(default='default.jpg', upload_to='covers')
     active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Landing(models.Model):
 class AboutUs(models.Model):
     headline = models.CharField(max_length=20, null=False, unique=True)
     body = models.TextField(max_length=1000, null=False)
-    cover_image = models.ImageField(default='default.jpg, upload_to='about_us')
+    cover_image = models.ImageField(default='default.jpg', upload_to='about_us')
 
     def __str__(self):
         return self.headline
@@ -21,7 +21,7 @@ class AboutUs(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True)
     description = models.TextField(max_length=500, null=False)
-    cover_image = models.ImageField(default='default.jpg, upload_to='products')
+    cover_image = models.ImageField(default='default.jpg', upload_to='products')
     url = models.URLField(unique=True)
 
     def __str__(self):
@@ -30,7 +30,7 @@ class Product(models.Model):
 class UpcomingEvents(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True)
     description = models.TextField(max_length=500, null=False)
-    cover_image = models.ImageField(default='default.jpg, upload_to='upcoming_events')
+    cover_image = models.ImageField(default='default.jpg', upload_to='upcoming_events')
     url = models.URLField(unique=True)
 
     def __str__(self):
