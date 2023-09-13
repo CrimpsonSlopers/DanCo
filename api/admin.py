cf1091c27423cf84233a574f496dbbcdb4122ef3
John Rogers <69231460+CrimpsonSlopers@users.noreply.github.com>
2023-09-13 18:14:09 -0500

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+admin.site.register(Landing)
+admin.site.register(Aboutus)
+admin.site.register(Products)
+admin.site.register(UpcomingEvents)
