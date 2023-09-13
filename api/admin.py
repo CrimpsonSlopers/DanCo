15e5b9d2e269fd7906e1396d9e6a2e3b3f4f798a
John Rogers <69231460+CrimpsonSlopers@users.noreply.github.com>
2023-09-13 18:14:41 -0500

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -3,6 +3,6 @@ from api.models import *
 
 # Register your models here.
 admin.site.register(Landing)
-admin.site.register(Aboutus)
+admin.site.register(AboutUs)
 admin.site.register(Products)
 admin.site.register(UpcomingEvents)
